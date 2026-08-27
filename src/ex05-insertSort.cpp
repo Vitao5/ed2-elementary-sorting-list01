25ed2f27e7114769057bf68815c9fa1ac70a326a
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-08-26 22:18:54 -0300

    Implement insertion sort algorithm

--- src/ex05-insertSort.cpp
+++ src/ex05-insertSort.cpp
@@ -1,3 +1,34 @@
 //
 // Created by vitin on 19/08/2026.
 //
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        int chave = a[i];
+        int j = i + 1;
+        while (j <= n - 1 && a[j] > chave) {
+            a[j - 1] = a[j];
+            j++;
+        }
+        a[j - 1] = chave;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", a[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+
+    return 0;
+}
